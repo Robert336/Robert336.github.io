56b068f0a898374c77af710bc9273b98fcceeb8a
Robert336 <rmazza33@gmail.com>
2021-01-01 12:09:43 -0500

    update

---
+++ LigmaCheats/website/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h className="App-header">
+        Hello World,
+        <br></br>
+        I'm Robert Mazza
+      </h>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ LigmaCheats/website/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from "./App";
+import Nav from './components/topNav'
+
+ReactDOM.render(
+    <Nav/>,
+    document.getElementById("nav")
+);
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
